38a462adad2abd0a1259ee40b3e0af4c0a0b8085
Steve Fosdal <steve@fosdal.net>
2014-04-08 12:50:54 -0500

    updated test to reflect jmxDomain

--- src/test/java/io/initium/camel/component/metrics/MetricsTest.java
+++ src/test/java/io/initium/camel/component/metrics/MetricsTest.java
@@ -37,10 +37,10 @@ public class MetricsTest extends CamelTestSupport {
 						exchange.getIn().setBody(stringBuilder.toString());
 					}
 				};
-				// from("timer://namedTimer?period=100").to("metrics://sampleWithoutTiming?context=context01");
-				// from("timer://namedTimer?period=200").to("metrics://sampleWithoutTiming?context=context02").delay(1000);
-				// from("timer://namedTimer?period=100").to("metrics://sampleWithTiming?context=context02&timing=start").delay(1000).to("metrics://sampleWithTiming?timing=stop");
-				// from("timer://namedTimer?period=100").to("metrics://sampleWithCounter1?context=context03&counterDelta=3&counterName=MyName");
+				// from("timer://namedTimer?period=100").to("metrics://sampleWithoutTiming?jmxDomain=context01");
+				// from("timer://namedTimer?period=200").to("metrics://sampleWithoutTiming?jmxDomain=context02").delay(1000);
+				// from("timer://namedTimer?period=100").to("metrics://sampleWithTiming?jmxDomain=context02&timing=start").delay(1000).to("metrics://sampleWithTiming?timing=stop");
+				// from("timer://namedTimer?period=100").to("metrics://sampleWithCounter1?jmxDomain=context03&counterDelta=3&counterName=MyName");
 				// from("timer://namedTimer?period=100").setHeader("testCounterDelta",
 				// simple("2")).to("metrics://sampleWithCounter2?counterDelta=${in.header.testCounterDelta}");
 				// from("timer://namedTimer?period=100").to("metrics://sampleWithHistogram1?histogramValue=3");
@@ -56,15 +56,15 @@ public class MetricsTest extends CamelTestSupport {
 	}
 
 	@Test
-	// @Ignore
+	@Ignore
 	public void theSecondTest() throws Exception {
 		this.context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				// from("timer://namedTimer?period=100").to("metrics://sample?context=context1&enableInternalTimer=true");
-				// from("timer://namedTimer?period=100").to("metrics://sample?context=context2&durationUnit=seConds");
-				from("timer://namedTimer?period=1000").to("metrics://sample?context=context3&histogramValue=1");
-				// from("timer://namedTimer?period=100").to("metrics://sample?context=context4&histogramValue=1&histogramReservoir=slidingTimewindow&slidingTimeWindowDuration=10&slidingTimeWindowDurationUnit=seconds");
+				// from("timer://namedTimer?period=100").to("metrics://sample?jmxDomain=context1&enableInternalTimer=true");
+				// from("timer://namedTimer?period=100").to("metrics://sample?jmxDomain=context2&durationUnit=seConds");
+				from("timer://namedTimer?period=1000").to("metrics://sample?jmxDomain=context3&histogramValue=1");
+				// from("timer://namedTimer?period=100").to("metrics://sample?jmxDomain=context4&histogramValue=1&histogramReservoir=slidingTimewindow&slidingTimeWindowDuration=10&slidingTimeWindowDurationUnit=seconds");
 			}
 		});
 		this.context.start();
