1072f2eb0699411ab7e5f8240dce35656c7f100c
Steve Fosdal <steve@fosdal.net>
2014-04-21 09:13:55 -0500

    adding logging

--- src/test/java/io/initium/camel/component/metrics/jmx/reporter/CamelMetricsJmxReporterTestSupport.java
+++ src/test/java/io/initium/camel/component/metrics/jmx/reporter/CamelMetricsJmxReporterTestSupport.java
@@ -6,47 +6,56 @@ import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CamelMetricsJmxReporterTestSupport extends CamelTestSupport {
-	MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
 
-	public boolean verifyObjectNameIsRegistered(String objectNameName) {
+	// logging
+	private static final String	SELF		= Thread.currentThread().getStackTrace()[1].getClassName();
+	private static final Logger	LOGGER		= LoggerFactory.getLogger(SELF);
+
+	// fields
+	MBeanServer					mbeanServer	= ManagementFactory.getPlatformMBeanServer();
+
+	public <T> T getValue(final String objectNameName, final String attributeName, final Class<T> type) {
 		try {
 			ObjectName objectName = new ObjectName(objectNameName);
-			return mbeanServer.isRegistered(objectName);
+			Object value = this.mbeanServer.getAttribute(objectName, attributeName);
+			return type.cast(value);
 		} catch (Exception e) {
-			return false;
+			return null;
 		}
 	}
 
-	public boolean verifyAttributeExists(String objectNameName, String attributeName) {
+	public boolean verifyAttributeExists(final String objectNameName, final String attributeName) {
 		try {
 			ObjectName objectName = new ObjectName(objectNameName);
-			mbeanServer.getAttribute(objectName, attributeName);
+			this.mbeanServer.getAttribute(objectName, attributeName);
 			return true;
 		} catch (Exception e) {
 			return false;
 		}
 	}
-	
-	public boolean verifyAttributeValueLong(String objectNameName, String attributeName, Long expected) {
+
+	public boolean verifyAttributeValueLong(final String objectNameName, final String attributeName, final Long expected) {
 		try {
 			ObjectName objectName = new ObjectName(objectNameName);
-			Object value = mbeanServer.getAttribute(objectName, attributeName);
-			Long doubleValue = (Long) value;
-			return doubleValue.equals(expected);
+			Object value = this.mbeanServer.getAttribute(objectName, attributeName);
+			Long longValue = (Long) value;
+			return longValue.equals(expected);
 		} catch (Exception e) {
+			LOGGER.error("could not convert to Long", e);
 			return false;
 		}
 	}
-	
-	public <T> T getValue(String objectNameName, String attributeName, Class<T> type) {
+
+	public boolean verifyObjectNameIsRegistered(final String objectNameName) {
 		try {
 			ObjectName objectName = new ObjectName(objectNameName);
-			Object value = mbeanServer.getAttribute(objectName, attributeName);
-			return type.cast(value);
+			return this.mbeanServer.isRegistered(objectName);
 		} catch (Exception e) {
-			return null;
+			return false;
 		}
 	}
 
